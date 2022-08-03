da9e998a312e5d94b0dc969fdf97512b9bdeb795
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-08-03 08:52:51 +0400

    Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+const Card = (props) => {
+  const { img, website, description, price } = props
+  return (
+    <div className="card-wrapper">
+      <img src={img} alt="" />
+      <div className="card-description">
+        <p className="card-description-website">{website}</p>
+        <p className="card-description-description">{description}</p>
+        <p className="card-description-price">{price}</p>
+      </div>
+    </div>
+  )
+}
+
+const Heading = () => {
+  return (
+    <div>
+      <h1>Artahanum</h1>
+    </div>
+  )
+}
+
+const Body = () => {
+  return <div className="cards-wrapper">
+    <Card
+      img={'https://onex.am/images/deals/2362.png'}
+      website={'www.zara.com'}
+      description={'Black Dress'}
+      price={"15E"}
+    />
+    <Card
+    img={'https://onex.am/images/deals/2361.png'}
+    website={' www.asos.com '}
+    description={" Man's Pink Shirt "}
+    price={"9E"}
+    />
+    <Card 
+    img={'https://onex.am/images/deals/2362.png'}
+    website={'www.zara.com'}
+    description={'Black Dress'}
+    price={"15E"}
+    />
+  </div>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <Body />
     </div>
   );
 }
+
+//React.createElement('div')
+//document.creatElement('div')
 
 export default App;
